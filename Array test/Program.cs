4f3ebe33a16c66f9fbcf3a9145c493512a2fabc1
ReyesCS <reyes1561@yahoo.com>
2017-07-16 19:50:06 -0700

    First Commit

---
+++ Array test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a phrase I reversed it on purpose to show you what happens
+            string phrase = "!hsup elttil a si sekat ti lla ytivarg ekil si wonk uoy sa ssendaM ";
+
+            // creating a array using all the charaters from the phrase and reverse them
+            char[] charArray = phrase.ToCharArray();
+            Array.Reverse(charArray);
+
+            // grabs all letters in a reverse order and will display to the screen the entire phrase in reverse
+            foreach (char phraseChar in charArray)
+            {
+                Console.Write(phraseChar);
+            }
+            Console.ReadLine();
+        }
+    }
+}
